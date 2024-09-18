6116bad832e0aa79500bd889d23bebf581566a7d
AndrewDRichter <andrewrichter1@hotmail.com>
2024-09-18 11:23:06 -0400

    nem implementations

--- file_handling.py
+++ file_handling.py
@@ -1,6 +1,27 @@
 import os
+from pathlib import Path
 
+def continue_input(func):
+    
+    def does_it(*args, **kwargs):
+        func(*args, **kwargs)
+        input("Continuar...")
+    
+    return does_it
 
+@continue_input
+def empty_file(filepath:str="") -> bool:
+    print(Path(__file__).parent)
+
+@continue_input
+def file_exists():
+    try:
+        existe = Path(__file__).parent.joinpath("files").exists()
+        
+    except Exception as e:
+        print(e)
+
+@continue_input
 def cadastra_usuario():
     try:
         user_name = str(input("Nombre de usuario: "))
@@ -13,21 +34,25 @@ def cadastra_usuario():
         if empty:
             print("vazio")
 
-        with open("datos.txt", "w") as f:
+        with open(str(Path(__file__).parent) + r"\files\datos.txt", "w") as f:
             f.write(user_name)
             f.write(", ")
             f.write(user_password)
             f.close()
         
-        input("Continuar...")
+        # input("Continuar...")
     except Exception as e:
         print(f"Exception: {e}")
-        input("Continuar")
+        # input("Continuar")
 
+@continue_input
 def visualiza_usuarios():
-    with open("datos.txt", "r") as f:
-        print(len(f.read()))
-        input("Continuar...")
+    try:
+        with open("datos.txt", "r") as f:
+            print(len(f.read()))
+            # input("Continuar...")
+    except Exception as e:
+        print(e)
 
 def menu():
     os.system("cls")
@@ -39,6 +64,8 @@ def menu():
     
 while True:
     r = menu()
+    # empty_file()
+    file_exists()
     match r:
         case '1':
             cadastra_usuario()
@@ -46,3 +73,4 @@ while True:
             visualiza_usuarios()
         case _:
             print("Não existe essa opção!")
+            input("Continuar...")
